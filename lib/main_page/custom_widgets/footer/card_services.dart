19fafbcec0602769c358f9498d75c9f7901cd2cb
Borschik0 <72044379+Borschik0@users.noreply.github.com>
2021-11-24 20:33:22 +0200

    12

---
+++ lib/main_page/custom_widgets/footer/card_services.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CardServices extends StatelessWidget {
+  const CardServices({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+
+      ),
+    );
+  }
+}
